706652cb63b2b430efd5dc0e842f3b5d125d33ab
sandip-ACSM <sandip.karar@augmentedscm.com>
2020-05-17 07:02:19 +0530

    Package version changed

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 var notesRouter = require("./note-app/routes/note.routes");
 var logger = require("morgan");
-require('hbs')
+require("hbs")
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const swaggerUi = require("swagger-ui-express");
@@ -12,11 +12,11 @@ const swaggerDocs = require("./swagger/swaggerDoc");
 // create express app
 const app = express();
 
-app.use(logger('combined'));
+app.use(logger("combined"));
 
 // view engine setup
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'note-app', 'views'))
+app.set("view engine", "hbs")
+app.set("views", path.join(__dirname, "note-app", "views"))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -31,24 +31,25 @@ app.use("/notes-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Configuring the database
 const DATABASE_URL = process.env.DATABASE_URL;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
 mongoose.connect(DATABASE_URL, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log("Successfully connected to the database");
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
 });
 
 //Route Prefixes
-app.get('/', function (req, res) {
-    res.render('index', {
-        name: 'Sandip'
+app.get("/", function (req, res) {
+    res.render("index", {
+        name: "Sandip"
     });
 });
 app.use("/notes", notesRouter);
